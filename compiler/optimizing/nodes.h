46094168fe6a55cc4045c00eef3a3ad5c91ea9c1
Artem Serov <artem.serov@linaro.org>
2020-02-24 18:51:42 +0000

    ART: Introduce predicated vector instructions
    This CL introduces a minimal changes to the IR to support
    autovectorization with use of predicated execution of SIMD
    instructions (e.g. Arm SVE).
    
    Test: test-art-target, test-art-host.
    Change-Id: Ibb7c5520fec6b858fb29f0dde19ec65501831a3a
    Signed-off-by: Akash <akashniki@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- compiler/optimizing/nodes.h
+++ compiler/optimizing/nodes.h
@@ -1565,6 +1565,9 @@ class HLoopInformationOutwardIterator : public ValueObject {
   M(VecDotProd, VecOperation)                                           \
   M(VecLoad, VecMemoryOperation)                                        \
   M(VecStore, VecMemoryOperation)                                       \
+  M(VecPredSetAll, VecPredSetOperation)                                 \
+  M(VecPredWhile, VecPredSetOperation)                                  \
+  M(VecPredCondition, VecOperation)                                     \
 
 #define FOR_EACH_CONCRETE_INSTRUCTION_COMMON(M)                         \
   FOR_EACH_CONCRETE_INSTRUCTION_SCALAR_COMMON(M)                        \
@@ -1625,7 +1628,8 @@ class HLoopInformationOutwardIterator : public ValueObject {
   M(VecOperation, Instruction)                                          \
   M(VecUnaryOperation, VecOperation)                                    \
   M(VecBinaryOperation, VecOperation)                                   \
-  M(VecMemoryOperation, VecOperation)
+  M(VecMemoryOperation, VecOperation)                                   \
+  M(VecPredSetOperation, VecOperation)
 
 #define FOR_EACH_INSTRUCTION(M)                                         \
   FOR_EACH_CONCRETE_INSTRUCTION(M)                                      \
